666fb508a43b3cf1ac1a5994d69a7fb7547c0abe
TANVIR HASAN <tanvirhasanbcse@gmail.com>
2023-09-19 11:15:48 +0600

    Update header.php

--- admin_new/layout/header.php
+++ admin_new/layout/header.php
@@ -15,7 +15,7 @@ include "../controller/config/functions.php";
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Admin | Global Center of Excellence</title>
+  <title>Admin | Basecamp Study</title>
   <link rel="icon" type="image/x-icon" href="../assets/upload/brand/fvicon.png">
   <link rel="stylesheet" href="../assets/css/vendor/bootstrap.min.css">
   <link rel="stylesheet" href="../assets/css/admin.css">
